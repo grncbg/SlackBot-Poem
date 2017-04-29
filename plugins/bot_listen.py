cccd36b5b5d92f1c0ee22a9ec2baa8a9260c44f2
Teruhiro Takahashi <grncbg@gmail.com>
2017-04-30 01:37:55 +0900

    UPDATE bot_listen.py

--- plugins/bot_listen.py
+++ plugins/bot_listen.py
@@ -1,15 +1,26 @@
 # coding: utf-8
 """ channelへ発言が来た時 """
-from slackbot.bot import listen_to
-import poemlist
-import select_random
 
-DIR = "../poem/poem.csv"
+from slackbot.bot import listen_to
+from slacker import Slacker
+import slackbot_settings
+from . import poemlist
+from . import select_random
+
+DIR = "poem/poem.csv"
 
 @listen_to("ポエム")
-def random_poem(message):
+def rondom_poem(message):
     """ ランダムにポエムを返す """
+    slack = Slacker(slackbot_settings.API_TOKEN)
     poem = poemlist.Poem(DIR)
     poems = poem.get_poems()
     random = select_random.SelectRandom(poems)
-    print(random)
+    name, poem = random.get_random_poem()
+    slack.chat.post_message(
+        "bot-test",
+        poem+" by @"+name,
+        username=name,
+        icon_emoji=":"+name+":",
+        link_names=True
+        )
