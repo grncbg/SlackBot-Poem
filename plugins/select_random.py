b6d0d9f48358aaa950b3f4537612b9f02181c696
Teruhiro Takahashi <grncbg@gmail.com>
2017-04-30 12:28:52 +0900

    FIX 乱数の範囲の修正
    生成される乱数が配列外を含んでいたため修正

--- plugins/select_random.py
+++ plugins/select_random.py
@@ -11,7 +11,7 @@ class SelectRandom:
     def get_random_poem(self):
         """ ランダムなポエムを返す """
         num = len(self.poem["name"])
-        rand = random.randint(0, num)
+        rand = random.randint(0, num-1)
         name = self.poem["name"][rand]
         poem = self.poem["poem"][rand]
         return (name, poem)
